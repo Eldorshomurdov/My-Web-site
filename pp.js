67c5c1e3b434da4b3a0efa56b386a5a81e59d2d1
Eldorshomurdov <81247251+Eldorshomurdov@users.noreply.github.com>
2021-04-28 07:19:20 +0300

    Add files via upload

---
+++ pp.js
@@ -0,0 +1,69 @@
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = [ " Men Nodirxonov Qodirxon"," Frontend Developer man."];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    }
+    else {
+        cursorSpan.classList.remove("typing");
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    }
+    else {
+        cursorSpan.classList.remove("typing");
+        textArrayIndex++;
+        if(textArrayIndex>=textArray.length) textArrayIndex=0;
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+$(document).ready(function(){
+    $('.p-box').tilt({
+        scale:0.8,
+    })
+});
+
+
+window.addEventListener("DOMContentLoaded",function (){
+
+    const scrollNav = document.querySelector('nav');
+    window.addEventListener('scroll',() => {
+        scrollNav.classList.toggle('shadow',window.scrollY > 0)
+    });
+
+    const scrollBtn = document.querySelector('.scrollToTop-btn');
+    window.addEventListener('scroll',() => {
+        scrollBtn.classList.toggle('active',window.scrollY > 500)
+    });
+});
+
+
+
+function scrollToTop(){
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    })
+}
